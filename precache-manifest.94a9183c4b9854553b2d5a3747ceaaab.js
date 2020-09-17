5069016c3a2f9e50a56e0abbc4d3ec91a0407353
ArnoldOmondi <anomondy@gmail.com>
2020-09-17 22:04:32 +0300

    Updates

---
+++ precache-manifest.94a9183c4b9854553b2d5a3747ceaaab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f66acfc2da03be7e8a3efe7c30b74c",
+    "url": "/zombie-land/index.html"
+  },
+  {
+    "revision": "4138ce514ab93a9cd082",
+    "url": "/zombie-land/static/css/main.ea03b0bb.chunk.css"
+  },
+  {
+    "revision": "41e482886775d383f4e9",
+    "url": "/zombie-land/static/js/2.ef7b0cbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/zombie-land/static/js/2.ef7b0cbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4138ce514ab93a9cd082",
+    "url": "/zombie-land/static/js/main.110eb691.chunk.js"
+  },
+  {
+    "revision": "039b67e7aea461449326",
+    "url": "/zombie-land/static/js/runtime-main.8356cb4d.js"
+  }
+]);
